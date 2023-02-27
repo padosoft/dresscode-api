538486bc93d3af4b27ab609b4b9d4a7bfd163c46
Marco Bramato <bramatom@gmail.com>
2023-02-27 15:37:57 +0100

    :sparkle: add subscription_key

--- src/DressCodeKey.php
+++ src/DressCodeKey.php
@@ -14,15 +14,18 @@ public string $password;
 
 public string $jwt;
 
-public function __construct(string $username, string $password, string $hub_key)
+public function __construct(string $username, string $password, string $hub_key, string $subscription_key)
 {
     $this->username = $username;
     $this->password = $password;
     $this->hub_key = $hub_key;
 
     $url = DressCodeEndPoints::create()->postJwtEndpoint($hub_key);
+    $headers = [
+        'Ocp-Apim-Subscription-Key' => $subscription_key,
+    ];
+    $client = new Client(['headers' => $headers]);
 
-    $client = new Client();
     $data = [
         'data' => [
             'username' => $username,
